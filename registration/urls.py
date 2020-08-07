0f3fe925879a725a6f368318c2a4163d64d59a0a
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-07 16:36:51 +0300

    Added base.html & template inheritance

--- registration/urls.py
+++ registration/urls.py
@@ -7,11 +7,19 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('dashboard/', views.dashboard, name='dashboard'),
 
-    path ('reset_password/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password.html'), name="password_reset"),
+    path ('reset_password/', auth_views.PasswordResetView.as_view(
+    						template_name='registration/reset_password.html'), 
+    						name="password_reset"),
 
-    path ('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_sent.html'), name="password_reset_done"),
+    path ('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
+    						template_name='registration/password_reset_sent.html'), 
+    						name="password_reset_done"),
 
-    path ('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_id.html'), name="password_reset_confirm"),
+    path ('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    						template_name='registration/password_reset_id.html'), 
+    						name="password_reset_confirm"),
 
-    path ('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_com.html'), name="password_reset_complete"),
+    path ('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+    						template_name='registration/password_reset_com.html'), 
+    						name="password_reset_complete"),
 ]
