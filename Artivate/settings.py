c6b68c3226f8bd8fad16023ea9c3f455e452ec52
CleoMugs <cleophas.mugeni@gmail.com>
2020-08-21 20:27:49 +0300

    added .env file

--- Artivate/settings.py
+++ Artivate/settings.py
@@ -139,12 +139,24 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, './static')]
 LOGIN_REDIRECT_URL = 'dashboard'
 
 # SMTP CONFIGURATION
+'''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
+'''
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
 SITE_ID = 1
 LOGIN_REDIRECT_URL = "dashboard"
